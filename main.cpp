58e9514019900fbe4cab18b1c9295884d4b3ac67
Javier <javier.fuentesm@hotmail.com>
2020-02-23 19:43:58 -0600

    Correccion del main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-git
+#include <iostream>
+#include "ListaCriba.h"
+
+int main() {
+
+    ListaCriba lista;
+    int n = 20;
+    for (int i = 2; i < n+1; i++) {
+        lista.addNumero(i);
+    }
+    lista.algoritmo();
+    lista.printPrimos();
+    return 0;
+}
